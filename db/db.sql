f607e9cc816ca6aed4503eb0a4f4da35120ce9f8
Hossein Fakharzadeh <fakharzadehh@yahoo.com>
2023-12-15 18:04:50 +0330

    feat: added db sql

---
+++ db/db.sql
@@ -0,0 +1,18 @@
+CREATE TABLE
+    users(
+        id int NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        username TEXT NOT NULL,
+        password TEXT NOT NULL,
+        created_at DATE DEFAULT NOW(),
+        updated_at DATE DEFAULT NOW()
+    );
+
+CREATE TABLE
+    baskets(
+        id int NOT NULL PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+        data VARCHAR(2048) NOT NULL,
+        user_id int NOT NULL,
+        state TEXT not null,
+        created_at DATE DEFAULT NOW(),
+        updated_at DATE DEFAULT NOW()
+    );
