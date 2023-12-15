2463e240147d26bf334e9a4fe38d0a4b90557bd3
Hossein Fakharzadeh <fakharzadehh@yahoo.com>
2023-12-15 18:06:09 +0330

    feat: added paylods

---
+++ domain/payloads/generic.go
@@ -0,0 +1,5 @@
+package payloads
+
+type GenericSuccessResponse struct {
+	Success bool `json:"success,omitempty"`
+}
---
+++ domain/payloads/user.go
@@ -0,0 +1,40 @@
+package payloads
+
+import (
+	"time"
+
+	"github.com/FakharzadehH/BasketInGo/domain"
+)
+
+type SignUpRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type LoginRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type LoginResponse struct {
+	Token     string      `json:"token"`
+	ExpiresAt *time.Time  `json:"expires_at"`
+	User      domain.User `json:"user"`
+}
+
+type CreateBasketRequest struct {
+	Data  string      `json:"data"`
+	State basketState `json:"state"`
+}
+
+type UpdateBasketRequest struct {
+	Data  string      `json:"data"`
+	State basketState `json:"state"`
+}
+
+type basketState string
+
+const (
+	basketStatePending  basketState = "PENDING"
+	basketStateComplete basketState = "COMPLETED"
+)
