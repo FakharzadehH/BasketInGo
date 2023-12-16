4df65917fe1100557ec5fc3afba9a93cfcb14120
Hossein Fakharzadeh <fakharzadehh@yahoo.com>
2023-12-16 03:56:39 +0330

    fix: deleted basket service

--- service/registry.go
+++ service/registry.go
@@ -2,23 +2,18 @@ package service
 
 import (
 	"github.com/FakharzadehH/BasketInGo/internal/repository"
-	"github.com/FakharzadehH/BasketInGo/service/basket"
 	"github.com/FakharzadehH/BasketInGo/service/user"
 )
 
 type Services struct {
-	User   User
-	Basket Basket
+	User User
 }
 
 func NewServices(repos *repository.Repositories) *Services {
 	return &Services{
-		User:   user.New(repos),
-		Basket: basket.New(repos),
+		User: user.New(repos),
 	}
 }
 
 type User interface {
-}
-type Basket interface {
 }
