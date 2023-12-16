7ed0e6f3f0a7502b4f6eb4f5e70108048eb9ea18
Hossein Fakharzadeh <fakharzadehh@yahoo.com>
2023-12-16 03:37:44 +0330

    feat: added basket repo

--- internal/repository/basket.go
+++ internal/repository/basket.go
@@ -1,6 +1,9 @@
 package repository
 
-import "gorm.io/gorm"
+import (
+	"github.com/FakharzadehH/BasketInGo/domain"
+	"gorm.io/gorm"
+)
 
 type basketRepository struct {
 	db *gorm.DB
@@ -8,4 +11,21 @@ type basketRepository struct {
 
 func newBasketRepository(db *gorm.DB) *basketRepository {
 	return &basketRepository{db: db}
+}
+
+func (b *basketRepository) GetBasketByID(id uint, basket *domain.Basket) error {
+	return b.db.First(basket, id).Error
+}
+
+func (b *basketRepository) GetByUserID(user_id uint) ([]domain.Basket, error) {
+	baskets := []domain.Basket{}
+	return baskets, b.db.Where("user_id = ?", user_id).Find(&baskets).Error
+}
+
+func (b *basketRepository) Upsert(basket *domain.Basket) error {
+	return b.db.Save(basket).Error
+}
+
+func (b *basketRepository) Delete(id uint) error {
+	return b.db.Delete(&domain.Basket{}, id).Error
 }
