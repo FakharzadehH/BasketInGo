2bba1d08cd30412b2946e5481686965088e27999
Hossein Fakharzadeh <fakharzadehh@yahoo.com>
2023-12-16 03:37:59 +0330

    feat: added user repo functions

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -1,6 +1,9 @@
 package repository
 
-import "gorm.io/gorm"
+import (
+	"github.com/FakharzadehH/BasketInGo/domain"
+	"gorm.io/gorm"
+)
 
 type userRepository struct {
 	db *gorm.DB
@@ -8,4 +11,16 @@ type userRepository struct {
 
 func newUserRepository(db *gorm.DB) *userRepository {
 	return &userRepository{db: db}
+}
+
+func (u *userRepository) GetByID(id uint, user *domain.User) error {
+	return u.db.First(user, id).Error
+}
+
+func (u *userRepository) GetByUsername(username string, user *domain.User) error {
+	return u.db.Where("username = ?", username).First(user).Error
+}
+
+func (u *userRepository) Insert(user *domain.User) error {
+	return u.db.Create(user).Error
 }
