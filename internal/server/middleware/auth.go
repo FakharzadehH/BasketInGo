561a3b74d22f762e5f3057aa75fe6c5e3f163f12
Hossein Fakharzadeh <fakharzadehh@yahoo.com>
2023-12-17 21:32:19 +0330

    feat: added Auth middleware func

--- internal/server/middleware/auth.go
+++ internal/server/middleware/auth.go
@@ -1,1 +1,32 @@
 package middleware
+
+import (
+	"strings"
+
+	"github.com/FakharzadehH/BasketInGo/domain"
+	"github.com/FakharzadehH/BasketInGo/internal/helpers"
+	"github.com/labstack/echo/v4"
+)
+
+func (m *Middlewares) Auth() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			header := strings.Split(c.Request().Header.Get("Authorization"), " ")
+			if len(header) != 2 {
+				return echo.NewHTTPError(401, "Unauthorized, invalid token")
+			}
+			token := header[1]
+			claims, err := helpers.ParseJWT(token)
+			if err != nil {
+				return echo.NewHTTPError(401, "Unauthorized, failed to parse token")
+			}
+
+			user := domain.User{}
+			if err := m.repos.User.GetByID(claims.ID, &user); err != nil {
+				return echo.NewHTTPError(401, "Unauthorize, failed to get user")
+			}
+			c.Set("user", &user)
+			return next(c)
+		}
+	}
+}
