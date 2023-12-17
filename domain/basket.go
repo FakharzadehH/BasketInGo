10c9a11fb10faba400b577e1a3217483b39fd76f
Hossein Fakharzadeh <fakharzadehh@yahoo.com>
2023-12-17 22:20:34 +0330

    feat: added value and scan funcs for Basket Data

--- domain/basket.go
+++ domain/basket.go
@@ -1,8 +1,14 @@
 package domain
 
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+)
+
 type Basket struct {
 	BaseModel
-	Data   string      `json:"data"`
+	Data   Data        `json:"data"`
 	State  BasketState `json:"state"`
 	UserID uint        `json:"user_id"`
 }
@@ -12,3 +18,19 @@ const (
 	BasketStatePending  BasketState = "PENDING"
 	BasketStateComplete BasketState = "COMPLETED"
 )
+
+type Data map[string]interface{}
+
+func (d Data) Value() (driver.Value, error) {
+	valueStr, err := json.Marshal(d)
+	return string(valueStr), err
+}
+
+func (d *Data) Scan(value interface{}) error {
+	bytes, ok := value.([]byte)
+	if !ok {
+		return errors.New("type assertion to []byte failed")
+	}
+
+	return json.Unmarshal(bytes, &d)
+}
