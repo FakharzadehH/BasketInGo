7b84066ef0c379c4bdefbfd1c92d22f211f21083
Hossein Fakharzadeh <fakharzadehh@yahoo.com>
2023-12-17 18:23:49 +0330

    feat: added auth hanlder funcs

---
+++ internal/server/handlers/auth.go
@@ -0,0 +1,52 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/FakharzadehH/BasketInGo/domain/payloads"
+	"github.com/labstack/echo/v4"
+)
+
+func (h *Handlers) AuthSignUp() echo.HandlerFunc {
+	type request struct {
+		payloads.SignUpRequest
+	}
+	type response struct {
+		payloads.GenericSuccessResponse
+	}
+	return func(c echo.Context) error {
+		var req request
+		if err := c.Bind(&req); err != nil {
+			return c.JSON(http.StatusBadRequest, nil)
+		}
+		res, err := h.svcs.User.SignUp(c.Request().Context(), req.SignUpRequest)
+		if err != nil {
+			return err
+		}
+		return c.JSON(200, &response{
+			GenericSuccessResponse: *res,
+		})
+	}
+}
+
+func (h *Handlers) AuthLogin() echo.HandlerFunc {
+	type request struct {
+		payloads.LoginRequest
+	}
+	type response struct {
+		payloads.LoginResponse
+	}
+	return func(c echo.Context) error {
+		var req request
+		if err := c.Bind(&req); err != nil {
+			return c.JSON(http.StatusBadRequest, nil)
+		}
+		res, err := h.svcs.User.Login(c.Request().Context(), req.LoginRequest)
+		if err != nil {
+			return err
+		}
+		return c.JSON(200, &response{
+			LoginResponse: *res,
+		})
+	}
+}
