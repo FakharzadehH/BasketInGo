1cb12c8bb6ccd20cc23409bf9ffb24860b87e2bd
Hossein Fakharzadeh <fakharzadehh@yahoo.com>
2023-12-17 21:33:13 +0330

    feat: added api routes

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -6,11 +6,16 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func routes(e *echo.Echo, handler *handlers.Handlers, m *middleware.Middlewares) {
-	// := e.Group("/basket")
+func routes(e *echo.Echo, h *handlers.Handlers, m *middleware.Middlewares) {
+	user := e.Group("/user", m.Auth())
+	user.GET("/basket", h.UserIndexBaskets())
+	user.GET("/basket/:id", h.UserGetBasket())
+	user.POST("/basket", h.UserCreateBasket())
+	user.PATCH("/basket/:id", h.UserUpdateBasket())
+	user.DELETE("/basket/:id", h.UserDeleteBasket())
 
-	// basket.GET("")
-	// basket.POST("")
-	// basket.PATCH("")
-	// basket.DELETE("")
+	auth := e.Group("/auth")
+	auth.POST("/signup", h.AuthSignUp())
+	auth.POST("/login", h.AuthLogin())
+
 }
